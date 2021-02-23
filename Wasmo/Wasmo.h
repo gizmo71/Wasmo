0570171f6243eaa7a76318b88488b72c11d3f33b
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-02-23 15:38:35 +0000

    Move module code from Wasmo to Examplezmo ready for the former to become a library.

---
+++ Wasmo/Wasmo.h
@@ -0,0 +1,10 @@
+#pragma once
+#pragma clang diagnostic ignored "-Wignored-attributes"
+
+#include <MSFS\MSFS.h>
+#include <MSFS\MSFS_WindowsTypes.h>
+#include <SimConnect.h>
+
+#include <INIReader.h>
+
+HANDLE g_hSimConnect;
