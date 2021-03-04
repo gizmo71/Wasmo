6818d52dc8733a26dabd48f3cebc561471643f85
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-03-04 19:31:41 +0000

    Fix handle-only data definition.

--- Spoilerzmo/Spoilerzmo.cpp
+++ Spoilerzmo/Spoilerzmo.cpp
@@ -11,8 +11,8 @@ enum GROUP_ID {
 };
 
 enum EVENT_ID {
-	EVENT_MORE_SPOILER = 7,
-	EVENT_LESS_SPOILER,
+	EVENT_MORE_SPOILER_TOGGLE = 7,
+	EVENT_LESS_SPOILER_ARM_GROUND,
 };
 
 enum DATA_DEFINE_ID {
@@ -30,10 +30,6 @@ struct SpoilersData {
 	INT32 spoilerHandle;
 };
 
-struct SpoilerHandleData {
-	INT32 spoilerHandle;
-};
-
 struct Spoilerzmo : Wasmo {
 	Spoilerzmo() : Wasmo("Spoilerzmo") { }
 	void init();
@@ -48,17 +44,17 @@ Wasmo* Wasmo::create() {
 void Spoilerzmo::init() {
 	cerr << "Spoilerzmo: init" << endl;
 
-	if (FAILED(SimConnect_MapClientEventToSimEvent(g_hSimConnect, EVENT_LESS_SPOILER, "SPOILERS_ARM_TOGGLE"))) {
+	if (FAILED(SimConnect_MapClientEventToSimEvent(g_hSimConnect, EVENT_LESS_SPOILER_ARM_GROUND, "SPOILERS_ARM_TOGGLE"))) {
 		cerr << "Spoilerzmo: couldn't map less/arm client event" << endl;
 	}
-	if (FAILED(SimConnect_MapClientEventToSimEvent(g_hSimConnect, EVENT_MORE_SPOILER, "SPOILERS_TOGGLE"))) {
+	if (FAILED(SimConnect_MapClientEventToSimEvent(g_hSimConnect, EVENT_MORE_SPOILER_TOGGLE, "SPOILERS_TOGGLE"))) {
 		cerr << "Spoilerzmo: couldn't map more/toggle client event" << endl;
 	}
 
-	if (FAILED(SimConnect_AddClientEventToNotificationGroup(g_hSimConnect, GROUP_SPOILERS, EVENT_LESS_SPOILER, TRUE))) {
+	if (FAILED(SimConnect_AddClientEventToNotificationGroup(g_hSimConnect, GROUP_SPOILERS, EVENT_LESS_SPOILER_ARM_GROUND, TRUE))) {
 		cerr << "Spoilerzmo: couldn't add less client event to group" << endl;
 	}
-	if (FAILED(SimConnect_AddClientEventToNotificationGroup(g_hSimConnect, GROUP_SPOILERS, EVENT_MORE_SPOILER, TRUE))) {
+	if (FAILED(SimConnect_AddClientEventToNotificationGroup(g_hSimConnect, GROUP_SPOILERS, EVENT_MORE_SPOILER_TOGGLE, TRUE))) {
 		cerr << "Spoilerzmo: couldn't add more client event to group" << endl;
 	}
 
@@ -76,7 +72,7 @@ void Spoilerzmo::init() {
 		cerr << "Spoilerzmo: couldn't map handle position to spoiler data" << endl;
 	}
 
-	if (FAILED(SimConnect_AddToDataDefinition(g_hSimConnect, DEFINITION_SPOILERS,
+	if (FAILED(SimConnect_AddToDataDefinition(g_hSimConnect, DEFINITION_SPOILER_HANDLE,
 		"SPOILERS HANDLE POSITION", "percent", SIMCONNECT_DATATYPE_INT32, 2.5))) {
 		cerr << "Spoilerzmo: couldn't map handle position to spoiler handle data" << endl;
 	}
@@ -85,13 +81,13 @@ void Spoilerzmo::init() {
 void Spoilerzmo::Handle(SIMCONNECT_RECV_EVENT* evt) {
 	cout << "Spoilerzmo: Received event " << evt->uEventID << " in group " << evt->uGroupID << endl;
 	switch (evt->uEventID) {
-	case EVENT_LESS_SPOILER:
+	case EVENT_LESS_SPOILER_ARM_GROUND:
 		cout << "Spoilerzmo: user has asked for less speedbrake (using the arm command)" << endl;
 		if (FAILED(SimConnect_RequestDataOnSimObject(g_hSimConnect, REQUEST_LESS_SPOILER, DEFINITION_SPOILERS, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_ONCE, SIMCONNECT_DATA_REQUEST_FLAG_DEFAULT, 0, 0, 0))) {
 			cerr << "Spoilerzmo: Could not request spoiler data as the result of an event" << endl;
 		}
 		break;
-	case EVENT_MORE_SPOILER:
+	case EVENT_MORE_SPOILER_TOGGLE:
 		cout << "Spoilerzmo: user has asked for more speedbrake (using the toggle command)" << endl;
 		if (FAILED(SimConnect_RequestDataOnSimObject(g_hSimConnect, REQUEST_MORE_SPOILER, DEFINITION_SPOILERS, SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_PERIOD_ONCE, SIMCONNECT_DATA_REQUEST_FLAG_DEFAULT, 0, 0, 0))) {
 			cerr << "Spoilerzmo: Could not request spoiler data as the result of an event" << endl;
@@ -105,35 +101,38 @@ void Spoilerzmo::Handle(SIMCONNECT_RECV_EVENT* evt) {
 void Spoilerzmo::Handle(SIMCONNECT_RECV_SIMOBJECT_DATA* pObjData) {
 	cout << "Spoilerzmo: RX data " << pObjData->dwRequestID << endl;
 	SpoilersData* spoilersData = (SpoilersData*)&pObjData->dwData;
-	SpoilerHandleData handleData;
+	INT32 handleData = -1;
 	switch (pObjData->dwRequestID) {
 	case REQUEST_LESS_SPOILER:
 		if (spoilersData->spoilerHandle > 0) {
-			handleData.spoilerHandle = max(spoilersData->spoilerHandle - 25, 0);
-			if (FAILED(SimConnect_SetDataOnSimObject(g_hSimConnect, DEFINITION_SPOILER_HANDLE, SIMCONNECT_OBJECT_ID_USER,
-				SIMCONNECT_DATA_REQUEST_FLAG_DEFAULT, 1, sizeof(SpoilerHandleData), &handleData))) {
-				cerr << "Spoilerzmo: Could not send spoiler handle position after reduction" << endl;
-			}
+			handleData = max(spoilersData->spoilerHandle - 25, 0);
 		} else if (spoilersData->spoilersArmed == 0) {
-			if (FAILED(SimConnect_TransmitClientEvent(g_hSimConnect, SIMCONNECT_OBJECT_ID_USER, EVENT_LESS_SPOILER, 0, GROUP_SPOILERS, SIMCONNECT_EVENT_FLAG_DEFAULT))) {
+			if (FAILED(SimConnect_TransmitClientEvent(g_hSimConnect, SIMCONNECT_OBJECT_ID_USER, EVENT_LESS_SPOILER_ARM_GROUND, 0, GROUP_SPOILERS, SIMCONNECT_EVENT_FLAG_DEFAULT))) {
 				cerr << "Spoilerzmo: Could not refire arm spoiler event" << endl;
 			}
 		}
 		break;
 	case REQUEST_MORE_SPOILER:
 		if (spoilersData->spoilersArmed != 0) {
-			if (FAILED(SimConnect_TransmitClientEvent(g_hSimConnect, SIMCONNECT_OBJECT_ID_USER, EVENT_LESS_SPOILER, 0, GROUP_SPOILERS, SIMCONNECT_EVENT_FLAG_DEFAULT))) {
+			if (FAILED(SimConnect_TransmitClientEvent(g_hSimConnect, SIMCONNECT_OBJECT_ID_USER, EVENT_LESS_SPOILER_ARM_GROUND, 0, GROUP_SPOILERS, SIMCONNECT_EVENT_FLAG_DEFAULT))) {
 				cerr << "Spoilerzmo: Could not refire arm spoiler event" << endl;
 			}
+			handleData = 0;
 		} else if (spoilersData->spoilerHandle <= 100) {
-			handleData.spoilerHandle = min(spoilersData->spoilerHandle + 25, 100);
-		}
-		if (FAILED(SimConnect_SetDataOnSimObject(g_hSimConnect, DEFINITION_SPOILER_HANDLE, SIMCONNECT_OBJECT_ID_USER,
-			SIMCONNECT_DATA_REQUEST_FLAG_DEFAULT, 1, sizeof(SpoilerHandleData), &handleData))) {
-			cerr << "Spoilerzmo: Could not send spoiler handle position after increase" << endl;
+			handleData = min(spoilersData->spoilerHandle + 25, 100);
 		}
 		break;
 	default:
 		cerr << "Spoilerzmo: Received unknown data: " << pObjData->dwRequestID << endl;
+	}
+
+	if (handleData != -1) {
+		cout << "Spoilerzmo: sending new handle position " << handleData << endl;
+		if (FAILED(SimConnect_SetDataOnSimObject(g_hSimConnect, DEFINITION_SPOILER_HANDLE, SIMCONNECT_OBJECT_ID_USER,
+			SIMCONNECT_DATA_REQUEST_FLAG_DEFAULT, 0, sizeof(handleData), &handleData))) {
+			cerr << "Spoilerzmo: Could not send spoiler handle position" << endl;
+		}
+	} else {
+		cout << "Spoilerzmo: no new handle position to send" << endl;
 	}
 }
