156da12ee9d60bb0ec4042bedabb0108ea7c47f8
Dave 'Gizmo' Gymer <gizmo@davegymer.org>
2021-04-19 18:48:31 +0100

    Remove VSpeeds stuff and just leave generic LVar poller.

--- Controlzmo/Controlzmo.cpp
+++ Controlzmo/Controlzmo.cpp
@@ -10,24 +10,10 @@ enum CONTROLZMO_ID {
 	EVENT_6HZ,
 	EVENT_1SEC,
 	EVENT_4SEC,
-	CLIENT_DATA_ID_VSPEED_CALLS,
-	CLIENT_DATA_DEFINITION_VSPEED_CALLS,
 	REQUEST_ID_LVAR_REQUEST,
 	CLIENT_DATA_ID_LVAR_REQUEST,
 	CLIENT_DATA_ID_LVAR_RESPONSE,
 	CLIENT_DATA_DEFINITION_LVAR,
-};
-
-// Safer to go from largest to smallest, to avoid padding anywhere but at the end...
-struct alignas(8) PMCallsClientData {
-	INT16 v1;
-	INT16 vr;
-	INT8 autobrake; // 0 for off, 1 for lo, 2 for med, 3 for max
-	INT8 weatherRadar; // 0 = 1, 1 = off, 2 = 2
-	INT8 pws;
-	INT8 tcas; // 0 standby, 1 TA, 2 TA/RA
-	INT8 tcasTraffic; // 0-3
-	INT8 padding[7];
 };
 
 const int32_t LVAR_POLL_ONCE = 0;
@@ -64,23 +50,6 @@ Wasmo* Wasmo::create() {
 void Controlzmo::init() {
 #if _DEBUG
 	cout << "Controlzmo: init" << endl;
-#endif
-	SimConnect_MapClientDataNameToID(g_hSimConnect, "Controlzmo.VSpeeds", CLIENT_DATA_ID_VSPEED_CALLS);
-	SimConnect_CreateClientData(g_hSimConnect, CLIENT_DATA_ID_VSPEED_CALLS, sizeof(PMCallsClientData), SIMCONNECT_CREATE_CLIENT_DATA_FLAG_READ_ONLY);
-#if _DEBUG
-	cout << "Controlzmo: created VSPeeds client data of size " << sizeof(PMCallsClientData) << "; #" << GetLastSentPacketID() << endl;
-#endif
-
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT16);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT16);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT8);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT8);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT8);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT8);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, SIMCONNECT_CLIENTDATATYPE_INT8);
-	SimConnect_AddToClientDataDefinition(g_hSimConnect, CLIENT_DATA_DEFINITION_VSPEED_CALLS, SIMCONNECT_CLIENTDATAOFFSET_AUTO, 7); // Padding
-#if _DEBUG
-	cout << "Controlzmo: added VSpeeds client data defs of size " << sizeof(LVarData) << "; #" << GetLastSentPacketID() << endl;
 #endif
 
 	SimConnect_MapClientDataNameToID(g_hSimConnect, "Controlzmo.LVarRequest", CLIENT_DATA_ID_LVAR_REQUEST);
@@ -184,54 +153,6 @@ void Controlzmo::Handle(SIMCONNECT_RECV_EVENT* eventData) {
 		break;
 	case EVENT_4SEC: {
 		CheckEachAndSend(LVAR_POLL_4SEC, true);
-
-		// IDs are -1 when not known
-		ID v1Id = check_named_variable("AIRLINER_V1_SPEED");
-		ID vrId = check_named_variable("AIRLINER_VR_SPEED");
-		ID autobrakeId = check_named_variable("XMLVAR_Autobrakes_Level");
-		ID radarId = check_named_variable("XMLVAR_A320_WeatherRadar_Sys");
-		ID pwsId = check_named_variable("A32NX_SWITCH_RADAR_PWS_Position");
-		ID tcasId = check_named_variable("A32NX_SWITCH_TCAS_Position");
-		ID tcasTrafficId = check_named_variable("A32NX_SWITCH_TCAS_Traffic_Position");
-
-		if (vrId == -1 && v1Id == -1 && autobrakeId == -1 && radarId == -1 && pwsId == -1 && tcasId == -1 && tcasTrafficId == -1) {
-#if _DEBUG
-			cout << "Controlzmo: no IDs found; skipping send" << endl;
-#endif
-			break;
-		}
-		PMCallsClientData clientData {
-			get_named_variable_value(v1Id),
-			get_named_variable_value(vrId),
-			get_named_variable_value(autobrakeId),
-			get_named_variable_value(radarId),
-			get_named_variable_value(pwsId),
-			get_named_variable_value(tcasId),
-			get_named_variable_value(tcasTrafficId),
-		};
-#if _DEBUG
-		cout << "Controlzmo: pilot monitoring calls data RX:"
-			<< " V1 " << clientData.v1 << " id " << v1Id
-			<< " VR " << clientData.vr << " id " << vrId
-			<< " autobrake " << (int)clientData.autobrake << " id " << autobrakeId << endl;
-		cout << "Controlzmo:\tRX continued:"
-			<< " radar " << (int)clientData.weatherRadar << " id " << radarId
-			<< " pws " << (int)clientData.pws << " id " << pwsId
-			<< " TCAS " << (int)clientData.tcas << " id " << tcasId
-			<< " traffic " << (int)clientData.tcasTraffic << " id " << tcasTrafficId
-			<< endl;
-#endif
-#if FALSE
-		// Phase isn't 100% reliable; goes through 3 and 4 on the first run, but sometimes a second takeoff run goes straight from 2 to 8, especially if using FLEX.
-		ID fwcPhaseId = check_named_variable("A32NX_FWC_FLIGHT_PHASE"); // See src/systems/systems/src/shared/mod.rs - 3 and 4 are the relevant ones
-		int fwcPhase = get_named_variable_value(fwcPhaseId);
-		cout << "Controlzmo: phase FWC >>>" << fwcPhase << "<<< id " << fwcPhaseId << endl;
-#endif
-		SimConnect_SetClientData(g_hSimConnect, CLIENT_DATA_ID_VSPEED_CALLS, CLIENT_DATA_DEFINITION_VSPEED_CALLS,
-			SIMCONNECT_CLIENT_DATA_SET_FLAG_DEFAULT, 0, sizeof(clientData), &clientData);
-#if _DEBUG
-		cout << "Controlzmo: set client data; #" << GetLastSentPacketID() << endl;
-#endif
 		break;
 	}
 	default:
